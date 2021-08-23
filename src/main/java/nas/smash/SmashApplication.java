6ae34e3f9e726ab600bce245944a353e07dbbec4
김대현 <kdh7037@naver.com>
2021-08-23 18:32:05 +0900

    heroku test

---
+++ src/main/java/nas/smash/SmashApplication.java
@@ -0,0 +1,12 @@
+package nas.smash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmashApplication.class, args);
+	}
+}
