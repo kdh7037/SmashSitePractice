d5b7155e0b4399c59ab2fb584cd04d6bfd7ea5e6
김대현 <kdh7037@naver.com>
2021-09-04 20:47:46 +0900

    spring mvc study

--- src/main/java/nas/smash/controller/NoticeController.java
+++ src/main/java/nas/smash/controller/NoticeController.java
@@ -1,26 +1,33 @@
 package nas.smash.controller;
 
+import nas.smash.entity.Notice;
 import nas.smash.repository.NoticeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/notice")
+@RequestMapping
 public class NoticeController {
     @Autowired
     NoticeRepository noticeRepository;
 
-    @GetMapping
-    public String check(){
-        return "welcome";
-    }
-
     @GetMapping(path = "/first")
     public List<String> getAllNotice(){
         return noticeRepository.getAllNotice();
+    }
+
+    @PostMapping
+    public String notice(NoticeForm form){
+        Notice notice = new Notice();
+        notice.setId(form.getId());
+
+        noticeService.join(notice);
+
+        return "redirect:/";
     }
 }
--- src/main/java/nas/smash/entity/Notice.java
+++ src/main/java/nas/smash/entity/Notice.java
@@ -27,4 +27,36 @@ public class Notice {
 
     @Column(length = 10000, nullable = false)
     private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
